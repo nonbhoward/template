ebce61ecf11d2fd85ee6f55099ecf10ee8b8b49c
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-29 17:11:11 -0400

    add: log dir and file name

--- constant/names.py
+++ constant/names.py
@@ -1,7 +1,9 @@
 # directories
 dir_configure = 'configure'
 dir_constant = 'constant'
+dir_logs = 'logs'
 dir_script = 'script'
 dir_setting = 'setting'
 # files
 file_logger_config = 'logger.yaml'
+file_logger_output = 'event.log'
