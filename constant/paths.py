1f47545e9cd7f31ec3c763a14ab860749aad6002
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-29 12:55:12 -0400

    init: base paths

---
+++ constant/paths.py
@@ -0,0 +1,42 @@
+from constant.names import dir_configure
+from constant.names import dir_constant
+from constant.names import dir_script
+from constant.names import dir_setting
+from os import getcwd
+from os.path import exists
+from pathlib import Path
+# global paths
+path_run = Path(getcwd())
+path_project = path_run.parent
+path_projects = path_project.parent
+path_home = path_projects.parent
+# project paths
+path_project_configure = Path(path_project, dir_configure)
+path_project_constant = Path(path_project, dir_constant)
+path_project_script = Path(path_project, dir_script)
+path_project_setting = Path(path_project, dir_setting)
+del dir_configure, dir_constant, dir_script, dir_setting
+# save paths to dict
+paths = dict()
+paths.update(
+    {
+        'global': {
+            'run':          path_run,
+            'project':      path_project,
+            'projects':     path_projects,
+            'home':         path_home},
+        'project': {
+            'configure':    path_project_configure,
+            'constant':     path_project_constant,
+            'script':       path_project_script,
+            'setting':      path_project_setting}
+    }
+)
+del path_run, path_project, path_projects, path_home
+del path_project_configure, path_project_constant, path_project_script, path_project_setting
+# verify paths
+for meta, meta_details in paths.items():
+    for path_name, path in meta_details.items():
+        print(f'{meta} path name is {path_name} for path {path}')
+del meta, meta_details, path_name, path
+pass
