d7d12353c05ab53d805f63ab3809623917c5e9e9
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-29 13:29:46 -0400

    wip: read yaml

--- configure/logging.py
+++ configure/logging.py
@@ -1,4 +1,9 @@
+from constant.paths import paths
 import yaml
+logger_config_file = paths['file']['logger config']
+with open(logger_config_file, 'r') as lcf:
+    contents = lcf.read()
+pass
 # TODO configure logger
 # planned data flow..
 #   1. yaml text on disk
