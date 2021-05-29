6d9980f6041655729e799755ddc2e43f236f5869
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-29 13:29:05 -0400

    init: keys

---
+++ constant/keys.py
@@ -0,0 +1,15 @@
+# global path keys
+global_ = 'global'
+run = 'run'
+project = 'project'
+projects = 'projects'
+home = 'home'
+# project path keys
+# project = 'project'  # intentionally commented
+configure = 'configure'
+constant = 'constant'
+script = 'script'
+setting = 'setting'
+# file path keys
+file = 'file'
+logger_config = 'logger config'
