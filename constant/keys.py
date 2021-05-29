bc140f7401f885f4567b6fe6c91d4ee8d8bcb610
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-29 17:10:41 -0400

    add: logger output key

--- constant/keys.py
+++ constant/keys.py
@@ -8,8 +8,10 @@ home = 'home'
 # project = 'project'  # intentionally commented
 configure = 'configure'
 constant = 'constant'
+logs = 'logs'
 script = 'script'
 setting = 'setting'
 # file path keys
 file = 'file'
 logger_config = 'logger config'
+logger_output = 'logger output'
