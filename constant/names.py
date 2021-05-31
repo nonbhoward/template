78108a6e4f0efcf0057dd498596ff6f5279568cd
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 12:32:00 -0400

    add: data & json keys

--- constant/names.py
+++ constant/names.py
@@ -1,9 +1,12 @@
 # directories
 dir_configure = 'configure'
 dir_constant = 'constant'
+dir_data = 'data'
 dir_logs = 'logs'
 dir_script = 'script'
 dir_setting = 'setting'
 # files
+file_json_dump = 'dump.json'
+file_json_load = 'load.json'
 file_logger_config = 'logger.yaml'
 file_logger_output = 'event.log'
