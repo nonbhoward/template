ff1bd62415c6cba60f6ffda4f1240e032e78e12f
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 00:28:07 -0400

    refactor: variable name

--- configure/logger.py
+++ configure/logger.py
@@ -4,9 +4,9 @@ import yaml
 # read the configuration
 logger_config_file = paths['file']['logger config']
 with open(logger_config_file, 'r') as lcf:
-    logger_config_dict = yaml.safe_load(lcf)
+    log_config = yaml.safe_load(lcf)
 del lcf, logger_config_file
 
 # direct the logger to use the logging path
-logger_config_dict['handlers']['file'].\
+log_config['handlers']['file']. \
     update(filename=paths['file']['logger output'])
