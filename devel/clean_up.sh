2c46e555ca5f722333c88a8d39368e447591ca90
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 15:03:35 -0400

    wip: init clean up

---
+++ devel/clean_up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# # # local variables
+path_script=$PWD
+
+# # # begin script
+clear && echo "launching clean up script"
+echo "script is running in the directory : "$path_script
+
+# # # get path parts
+# IFS, internal field separator
+IFS='/' read -ra PARTS <<< $path_script
+for part in "${PARTS[@]}"; do
+  echo $part
+done
+
+# # # delete logs
+# TODO
