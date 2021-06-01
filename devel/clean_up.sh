91441aab0e5b1a934c077c1f5aacc4b14bf2d670
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 20:47:31 -0400

    fix: bad style

--- devel/clean_up.sh
+++ devel/clean_up.sh
@@ -5,13 +5,13 @@ path_script=$PWD
 
 # # # begin script
 clear && echo "launching clean up script"
-echo "script is running in the directory : "$path_script
+echo "script is running in the directory : $path_script"
 
 # # # get path parts
 # IFS, internal field separator
-IFS='/' read -ra PARTS <<< $path_script
+IFS='/' read -ra PARTS <<< "$path_script"
 for part in "${PARTS[@]}"; do
-  echo $part
+  echo "$part"
 done
 
 # # # delete logs
