507debeef0ec1ed09352378e0792a01fbc9e0166
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-01 17:57:54 -0400

    del: file key in paths

--- configure/logger.py
+++ configure/logger.py
@@ -1,13 +1,15 @@
+from constant.names import fn_logger_config, fn_logger_output
 from constant.paths import paths
+from pathlib import Path
 import yaml
 
 # read the configuration
-yaml_config = paths['file']['yaml logger config']
+yaml_config = Path(paths['project']['setting'], fn_logger_config)
 with open(yaml_config, 'r') as yc:
     logger_configuration = yaml.safe_load(yc)
 del yc, yaml_config
 
 # update the logger to use the logging path
-logger_configuration['handlers']['file'].\
-    update(filename=paths['file']['logger output'])
+logger_configuration['handlers']['file'].update(
+    filename=Path(paths['project']['logs'], fn_logger_output))
 pass
