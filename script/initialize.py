43314228b4965be8594a76a76884563c38e1d5d7
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 21:00:52 -0400

    del: developer lines

--- script/initialize.py
+++ script/initialize.py
@@ -5,22 +5,6 @@ import logging.config
 # configure the logger
 logging.config.dictConfig(logger_configuration)
 del logger_configuration
-
-# get some loggers
-logr = logging.getLogger()
-log = logging.getLogger(__name__)
-
 if DEBUG:
-    # test root logger
-    print(f'testing the root logger')
-    logr.debug(f'root logger test debug message')
-    logr.info(f'root logger test info message')
-    logr.warning(f'root logger test warning message')
-    logr.error(f'root logger test error message')
-    # test module logger
-    print(f'testing the module logger')
-    log.debug(f'module logger test debug message')
-    log.info(f'module logger test info message')
-    log.warning(f'module logger test warning message')
-    log.error(f'module logger test error message')
+    pass
 pass
