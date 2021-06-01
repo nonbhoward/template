fdb9a896313280e032d4d560e5ae2378cbbc156c
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 23:49:58 -0400

    add: home and root paths

--- constant/paths.py
+++ constant/paths.py
@@ -7,13 +7,6 @@ from pathlib import Path
 path_to_running_script = Path(getcwd())
 path_to_project = path_to_running_script.parent
 path_to_projects = path_to_project.parent
-path_to_home = path_to_projects.parent
-path_to_desktop = Path(path_to_home, dir_desktop)
-path_to_documents = Path(path_to_home, dir_documents)
-path_to_downloads = Path(path_to_home, dir_downloads)
-path_to_music = Path(path_to_home, dir_music)
-path_to_pictures = Path(path_to_home, dir_pictures)
-path_to_videos = Path(path_to_home, dir_videos)
 # project paths
 path_to_project_configure_dir = Path(path_to_project, dir_configure)
 path_to_project_constant_dir = Path(path_to_project, dir_constant)
@@ -22,6 +15,26 @@ path_to_project_logs_dir = Path(path_to_project, dir_logs)
 path_to_project_script_dir = Path(path_to_project, dir_script)
 path_to_project_setting_dir = Path(path_to_project, dir_setting)
 del dir_configure, dir_constant, dir_script, dir_setting
+# home paths
+path_to_home = path_to_projects.parent
+path_to_desktop = Path(path_to_home, dir_desktop)
+path_to_documents = Path(path_to_home, dir_documents)
+path_to_downloads = Path(path_to_home, dir_downloads)
+path_to_music = Path(path_to_home, dir_music)
+path_to_pictures = Path(path_to_home, dir_pictures)
+path_to_videos = Path(path_to_home, dir_videos)
+# root paths
+path_to_root = Path(path_to_projects.parts[0])
+path_to_dev = Path(path_to_root, dev)
+path_to_etc = Path(path_to_root, etc)
+path_to_media = Path(path_to_root, media)
+path_to_mnt = Path(path_to_root, mnt)
+path_to_opt = Path(path_to_root, opt)
+path_to_proc = Path(path_to_root, proc)
+path_to_snap = Path(path_to_root, snap)
+path_to_tmp = Path(path_to_root, tmp)
+path_to_usr = Path(path_to_root, usr)
+path_to_var = Path(path_to_root, var)
 # file paths
 path_to_file_json_cache = Path(path_to_project_data_dir, fn_json)
 path_to_file_logger_config = Path(path_to_project_setting_dir, fn_logger_config)
@@ -33,14 +46,7 @@ paths.update(
         global_: {
             run:            path_to_running_script,
             project:        path_to_project,
-            projects:       path_to_projects,
-            home:           path_to_home,
-            desktop:        path_to_desktop,
-            documents:      path_to_documents,
-            downloads:      path_to_downloads,
-            music:          path_to_music,
-            pictures:       path_to_pictures,
-            videos:         path_to_videos},
+            projects:       path_to_projects},
         project: {
             configure:      path_to_project_configure_dir,
             constant:       path_to_project_constant_dir,
@@ -48,6 +54,26 @@ paths.update(
             logs:           path_to_project_logs_dir,
             script:         path_to_project_script_dir,
             setting:        path_to_project_setting_dir},
+        home: {
+            home:           path_to_home,
+            desktop:        path_to_desktop,
+            documents:      path_to_documents,
+            downloads:      path_to_downloads,
+            music:          path_to_music,
+            pictures:       path_to_pictures,
+            videos:         path_to_videos},
+        root: {
+            root:           path_to_root,
+            dev:            path_to_dev,
+            etc:            path_to_etc,
+            media:          path_to_media,
+            mnt:            path_to_mnt,
+            opt:            path_to_opt,
+            proc:           path_to_proc,
+            snap:           path_to_snap,
+            tmp:            path_to_tmp,
+            usr:            path_to_usr,
+            var:            path_to_var},
         file: {
             json_file:      path_to_file_json_cache,
             yaml_logger_config:  path_to_file_logger_config,
