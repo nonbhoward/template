da72ab79c6d7033a8e0eb3208129ec6db302e50f
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-05-31 23:49:37 -0400

    add: home and root keys

--- constant/names.py
+++ constant/names.py
@@ -1,16 +1,29 @@
-# directories
+# directories project
 dir_configure = 'configure'
 dir_constant = 'constant'
 dir_data = 'data'
+dir_logs = 'logs'
+dir_script = 'script'
+dir_setting = 'setting'
+# directories home
 dir_desktop = 'Desktop'
 dir_documents = 'Documents'
 dir_downloads = 'Downloads'
-dir_logs = 'logs'
 dir_music = 'Music'
 dir_pictures = 'Pictures'
-dir_script = 'script'
-dir_setting = 'setting'
 dir_videos = 'Videos'
+# directories root
+dir_root = 'root'
+dir_dev = 'dev'
+dir_etc = 'etc'
+dir_media = 'media'
+dir_mnt = 'mnt'
+dir_opt = 'opt'
+dir_proc = 'proc'
+dir_snap = 'snap'
+dir_tmp = 'tmp'
+dir_usr = 'usr'
+dir_var = 'var'
 # files
 fn_json = 'cache.json'
 fn_logger_config = 'logger.yaml'
