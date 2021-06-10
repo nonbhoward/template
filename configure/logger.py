69fbac3a069ff5cd6ce1f577d89d3ad54d426009
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-10 00:40:38 -0400

    refactor: constant var names

--- configure/logger.py
+++ configure/logger.py
@@ -1,15 +1,15 @@
-from constant.names import fn_logger_config, fn_logger_output
+from constant.names import fn_config_logger, fn_log
 from constant.paths import paths
 from pathlib import Path
 import yaml
 
 # read the configuration
-yaml_config = Path(paths['project']['setting'], fn_logger_config)
+yaml_config = Path(paths['project']['setting'], fn_config_logger)
 with open(yaml_config, 'r') as yc:
     logger_configuration = yaml.safe_load(yc)
 del yc, yaml_config
 
 # update the logger to use the logging path
 logger_configuration['handlers']['file'].update(
-    filename=Path(paths['project']['logs'], fn_logger_output))
+    filename=Path(paths['project']['logs'], fn_log))
 pass
