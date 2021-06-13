cc2df4449fbbcc35d588c25a35695a36a7b0277c
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-12 23:52:56 -0400

    refactor: format & categorize keys

--- constant/keys.py
+++ constant/keys.py
@@ -1,12 +1,16 @@
-# global path keys
+# path keys for file-specific definitions
+file = 'file'
+json_file = 'json cache'
+logger_output = 'logger output'
+yaml_logger_config = 'yaml logger config'
+
+# path keys for global path root definitions
 global_ = 'global'
 home = 'home'
 project = 'project'
 projects = 'projects'
 root = 'root'
 run = 'run'
-# file path keys
-file = 'file'
-json_file = 'json cache'
-logger_output = 'logger output'
-yaml_logger_config = 'yaml logger config'
+
+# path keys for path dict configuration
+setting = 'setting'
