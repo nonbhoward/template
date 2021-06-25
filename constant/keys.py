27ec9f73bca11fd1e5ca83dad11b8f18e6dc66fb
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-25 01:08:46 -0400

    add: keys : files, script

--- constant/keys.py
+++ constant/keys.py
@@ -1,5 +1,6 @@
 # path keys for file-specific definitions
 file = 'file'
+files = 'files'
 json_file = 'json cache'
 logger_output = 'logger output'
 yaml_logger_config = 'yaml logger config'
@@ -11,6 +12,7 @@ project = 'project'
 projects = 'projects'
 root = 'root'
 run = 'run'
+script = 'script'
 
 # path keys for path dict configuration
 setting = 'setting'
