df981e5ea79e009bf028eab13dabd1a17a88e9a9
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-06-26 22:24:02 -0400

    refactor: to attributes

--- configure/logger.py
+++ configure/logger.py
@@ -1,15 +1,15 @@
 from constant.names import fn_config_logger, fn_log
-from constant.paths import paths
+from constant.paths import path
 from pathlib import Path
 import yaml
 
 # read the configuration
-yaml_config = Path(paths['project']['setting'], fn_config_logger)
+yaml_config = Path(path.dirs['project']['setting'], fn_config_logger)
 with open(yaml_config, 'r') as yc:
     logger_configuration = yaml.safe_load(yc)
 del yc, yaml_config
 
 # update the logger to use the logging path
 logger_configuration['handlers']['file'].update(
-    filename=Path(paths['project']['logs'], fn_log))
+    filename=Path(path.dirs['project']['logs'], fn_log))
 pass
