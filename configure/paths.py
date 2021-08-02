57ec1958981d54fbcd6f90fbe0b8dcca47edcacd
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-02 18:16:35 -0400

    refactor: clarify var names

--- configure/paths.py
+++ configure/paths.py
@@ -1,4 +1,5 @@
-from constant.names import dn_setting, fn_config_paths
+from constant.names import dn_setting
+from constant.names import fn_paths_config
 from os import getcwd
 from os.path import exists
 from pathlib import Path
@@ -6,12 +7,11 @@ import yaml
 
 # read the configuration
 path_run = Path(getcwd())
-path_setting = Path(path_run.parent, 'setting')
-yaml_config = Path(path_setting, fn_config_paths)
-if not exists(yaml_config):
-    print(f'{yaml_config} not found')
+path_setting = Path(path_run.parent, dn_setting)
+path_to_paths_config = Path(path_setting, fn_paths_config)
+if not exists(path_to_paths_config):
+    print(f'{path_to_paths_config} not found')
     exit()
-with open(yaml_config, 'r') as yc:
-    paths_configuration = yaml.safe_load(yc)
-del yc, yaml_config
-pass
+with open(path_to_paths_config, 'r') as p_cfg:
+    paths_config = yaml.safe_load(p_cfg)
+del p_cfg, path_to_paths_config
