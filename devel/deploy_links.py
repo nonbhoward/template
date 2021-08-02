aae9277fa49c8b92784dbc9a7d8b4a93eae9bd9e
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-02 18:45:49 -0400

    refactor: link map moved to yaml

--- devel/deploy_links.py
+++ devel/deploy_links.py
@@ -1,44 +1,29 @@
+from configure.settings import config
 from constant.paths import path
-from os import mkdir, symlink
-from os.path import exists
 from pathlib import Path
+import os
 
-# developer provides a target project name
-target_project_name = 'target_project'  # FIXME, provide a target
-source_project_name = 'template'
+source_project_name = config['deploy']['source']
+target_project_name = config['deploy']['target']
+if not target_project_name:
+    print(f'no target name provided')
+    exit()
 
 # construct paths
-target_project = Path(path.paths['projects'], target_project_name)
 source_project = Path(path.paths['projects'], source_project_name)
+target_project = Path(path.paths['projects'], target_project_name)
 
 # inventory of links to be created
-sym_links = dict()
-sym_links['relative parts'] = dict()
-sym_links['relative parts'].update({
-    'configure paths':      ['configure', 'paths.py'],
-    'configure settings':   ['configure', 'settings.py'],
-    'constant keys':        ['constant', 'keys.py'],
-    'constant names':       ['constant', 'names.py'],
-    'constant paths':       ['constant', 'paths.py'],
-    'data path':            ['data'],
-    'devel clean up':       ['devel', 'clean_up.sh'],
-    'devel flags':          ['devel', 'flags.py'],
-    'log path':             ['logs'],
-    'script initialize':    ['script', 'initialize.py'],
-    'setting paths':        ['setting', 'paths.yaml'],
-    'setting settings':     ['setting', 'settings.yaml'],
-    'tool data management': ['tool', 'data_management.py']
-})
+relative_symbolic_links = dict(config['deploy']['relative symbolic links'])
 
 # build the links
-sym_links['links'] = dict()
-for link_label, link_parts in sym_links['relative parts'].items():
+for link_label, link_parts in relative_symbolic_links.items():
     # make the parent directory if needed
     target_dir = Path(target_project, link_parts[0])
-    if not exists(target_dir):
-        mkdir(target_dir)
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
     source_file = Path(source_project, * link_parts)
     target_file = Path(target_project, * link_parts)
-    if not exists(target_file):
+    if not os.path.exists(target_file):
         # execute the creation
-        symlink(src=source_file, dst=target_file)
+        os.symlink(src=source_file, dst=target_file)
