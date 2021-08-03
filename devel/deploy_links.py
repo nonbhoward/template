cbf9fa37d2ed22f0d323787d9a33484b23bfde4d
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-03 01:43:14 -0400

    feat: add deployment options, add delete option

--- devel/deploy_links.py
+++ devel/deploy_links.py
@@ -1,48 +1,64 @@
+from common.names import fn_deployment
 from configure.settings import config
-from common.paths import path
 from pathlib import Path
 import os
 import shutil
+import yaml
 
-
-source_project_name = config['deployment']['source']
-target_project_name = config['deployment']['target']
-if not target_project_name:
-    print(f'error, no target name provided')
-    exit()
-
-
-# construct paths
-source_project = Path(path.paths['projects'], source_project_name)
-target_project = Path(path.paths['projects'], target_project_name)
-
+source_project = config['path'].paths['project']
+projects = config['path'].dirs['projects']
 
 # inventory of links to be created
 links_to_create = dict(config['deployment']['link at target'])
 files_to_copy = dict(config['deployment']['copy to target'])
 
+# search projects for existence of ./setting/deployment.yaml
+deployment_details = dict()
+for project, project_path in projects.items():
+    path_to_deployment_config = Path(project_path, 'setting', fn_deployment)
+    if project_path == source_project:
+        continue  # skip self
+    if os.path.exists(path_to_deployment_config):
+        with open(path_to_deployment_config, 'r') as d_cfg:
+            deployment_config = yaml.safe_load(d_cfg)
+        deployment_details.update({project_path: deployment_config['receive']})
 
-# build the links
+# build and deploy the links
 for link_label, link_parts in links_to_create.items():
+    deploy = True
+    deployment_path = None
+    for deployment_path, deployment_elements in deployment_details.items():
+        deploy = True if link_label in deployment_elements else False
+    if not deploy or not deployment_path:
+        continue
     # make the parent directory if needed
-    target_dir = Path(target_project, link_parts[0])
+    target_dir = Path(deployment_path, link_parts[0])
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
     source_file = Path(source_project, * link_parts)
-    target_file = Path(target_project, * link_parts)
+    target_file = Path(deployment_path, * link_parts)
     if not os.path.exists(target_file):
         # execute the creation
         os.symlink(src=source_file, dst=target_file)
+        continue
+    if config['deployment']['mode'] == 'delete' and Path(target_file).is_symlink():
+        os.remove(path=target_file)
 
 
-# copy unlinked files
+# build and copy the unlinked files
 for copy_label, copy_parts in files_to_copy.items():
+    deploy = True
+    deployment_path = None
+    for deployment_path, deployment_elements in deployment_details.items():
+        deploy = True if copy_label in deployment_elements else False
+    if not deploy or not deployment_path:
+        continue
     # make the parent directory if needed
-    target_dir = Path(target_project, copy_parts[0])
+    target_dir = Path(deployment_path, copy_parts[0])
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
     source_file = Path(source_project, * copy_parts)
-    target_file = Path(target_project, * copy_parts)
+    target_file = Path(deployment_path, * copy_parts)
     if not os.path.exists(target_file):
         # execute the creation
         shutil.copy(src=source_file, dst=target_file)
