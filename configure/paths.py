50038de1fd711fc71e4e4918ecfd814ce0ffb6ce
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-02 20:36:31 -0400

    refactor: os import

--- configure/paths.py
+++ configure/paths.py
@@ -1,15 +1,14 @@
 from common.names import dn_setting
 from common.names import fn_paths_config
-from os import getcwd
-from os.path import exists
 from pathlib import Path
+import os
 import yaml
 
 # read the configuration
-path_run = Path(getcwd())
+path_run = Path(os.getcwd())
 path_setting = Path(path_run.parent, dn_setting)
 path_to_paths_config = Path(path_setting, fn_paths_config)
-if not exists(path_to_paths_config):
+if not os.path.exists(path_to_paths_config):
     print(f'{path_to_paths_config} not found')
     exit()
 with open(path_to_paths_config, 'r') as p_cfg:
