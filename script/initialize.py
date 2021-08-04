4cd01eb9a0dc6809617d20b530194a9dad416514
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-04 01:51:32 -0400

    config > app_config

--- script/initialize.py
+++ script/initialize.py
@@ -1,10 +1,10 @@
-from configure.settings import config
+from configure.settings import app_config
 from tool.socket_management import SocketHandler
 import logging.config
-logging.config.dictConfig(config['logger'])
+logging.config.dictConfig(app_config['logger'])
 log = logging.getLogger(__name__)
-if config.get('socket', None) and config['socket'].get('mode', None):
-    sh = SocketHandler(config['socket'])
+if app_config.get('socket', None) and app_config['socket'].get('mode', None):
+    sh = SocketHandler(app_config['socket'])
     if not sh.client and not sh.server:
         log.error(f'failed to instantiate socket')
 log.info(f'initialization complete')
