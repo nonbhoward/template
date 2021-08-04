93a1a8cce21d2b58c84d30e9403abab1b003d96d
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-04 02:07:54 -0400

    refactor: conditional imports

--- script/initialize.py
+++ script/initialize.py
@@ -1,10 +1,15 @@
 from configure.settings import app_config
-from tool.socket_management import SocketHandler
 import logging.config
+
 logging.config.dictConfig(app_config['logger'])
 log = logging.getLogger(__name__)
-if app_config.get('socket', None) and app_config['socket'].get('mode', None):
+
+if app_config.get('cache', None) and app_config['cache'].get('enabled', False):
+    from tool.data_management import CacheDataHandler
+if app_config.get('socket', None) and app_config['socket'].get('mode', None) != 'disabled':
+    from tool.socket_management import SocketHandler
     sh = SocketHandler(app_config['socket'])
     if not sh.client and not sh.server:
         log.error(f'failed to instantiate socket')
+
 log.info(f'initialization complete')
