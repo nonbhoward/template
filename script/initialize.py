ab773eb1fe9bd22126ceeeda62fce57178332fa1
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-07 15:51:58 -0400

    del: cache yaml check

--- script/initialize.py
+++ script/initialize.py
@@ -4,9 +4,7 @@ import logging.config
 logging.config.dictConfig(app_config['logger'])
 log = logging.getLogger(__name__)
 
-if app_config.get('cache', None) and app_config['cache'].get('enabled', False):
-    from tool.data_management import CacheDataHandler
-if app_config.get('socket', None) and app_config['socket'].get('mode', None) != 'disabled':
+if app_config.get('socket', None) and app_config['socket'].get('mode', None) == 'enabled':
     from tool.socket_management import SocketHandler
     sh = SocketHandler(app_config['socket'])
     if not sh.client and not sh.server:
