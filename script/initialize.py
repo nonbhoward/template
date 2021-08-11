65183c47497cbc6009aee65b2250832ad19103e6
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-11 02:28:11 -0400

    del: automatic socket instantiation

--- script/initialize.py
+++ script/initialize.py
@@ -6,9 +6,5 @@ log = logging.getLogger()  # root
 
 if app_config.get('socket', None) and app_config['socket'].get('mode', None) != 'disabled':
     from tool.socket_management import SocketHandler
-    sh = SocketHandler(app_config['socket'])
-    if not app_config['socket']['server'].get('instance', None) or \
-            not app_config['socket']['client'].get('instance', None):
-        log.error(f'failed to instantiate socket')
 
 log.info(f'initialization complete')
