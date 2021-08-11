49a59bd7880452583880b7335bcd32b983544cd7
nonbhoward <55259160+nonbhoward@users.noreply.github.com>
2021-08-11 00:00:30 -0400

    fix: bad key check, refactor: server/client locations

--- script/initialize.py
+++ script/initialize.py
@@ -4,10 +4,11 @@ import logging.config
 logging.config.dictConfig(app_config['logger'])
 log = logging.getLogger()  # root
 
-if app_config.get('socket', None) and app_config['socket'].get('mode', None) == 'enabled':
+if app_config.get('socket', None) and app_config['socket'].get('mode', None) != 'disabled':
     from tool.socket_management import SocketHandler
     sh = SocketHandler(app_config['socket'])
-    if not sh.client and not sh.server:
+    if not app_config['socket']['server'].get('instance', None) or \
+            not app_config['socket']['client'].get('instance', None):
         log.error(f'failed to instantiate socket')
 
 log.info(f'initialization complete')
